b687ef93ef1de901085bed6cd3d5f765ce151307
Miles Malerba <mmalerba@google.com>
2020-03-24 14:22:22 -0700

    add slide toggle

---
+++ projects/mat-slide-toggle/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <mat-slide-toggle>Slide me!</mat-slide-toggle>
+  `,
+  styles: []
+})
+export class AppComponent { }
