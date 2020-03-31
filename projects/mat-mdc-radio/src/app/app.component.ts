cdc32f41e5195f5aee223983db10fdcbda8a2438
Miles Malerba <mmalerba@google.com>
2020-03-31 15:07:48 -0700

    add comparison for radio

---
+++ projects/mat-mdc-radio/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <mat-radio-group>
+      <mat-radio-button value="1">Option 1</mat-radio-button>
+      <mat-radio-button value="2">Option 2</mat-radio-button>
+    </mat-radio-group>
+  `,
+  styles: []
+})
+export class AppComponent { }
