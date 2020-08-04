feae26859c601be1817727b25bac432d406726f1
Paul Gschwendtner <paulgschwendtner@gmail.com>
2020-08-04 17:50:06 +0200

    Add size test for MDC-based dialog

---
+++ projects/mat-dialog/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    NoopAnimationsModule,
+    BrowserModule,
+    MatDialogModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ projects/mat-mdc-dialog/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import {Component, TemplateRef, ViewChild} from '@angular/core';
+import {MatDialog} from '@angular/material-experimental/mdc-dialog';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <button (click)="open()">Open</button>
+
+    <ng-template>
+      <h2 matDialogTitle>Title</h2>
+      <mat-dialog-content>Content</mat-dialog-content>
+      <mat-dialog-actions>
+        <button matDialogClose>Close</button>
+      </mat-dialog-actions>
+    </ng-template>
+  `,
+})
+export class AppComponent {
+  @ViewChild(TemplateRef) template: TemplateRef<never>;
+
+  constructor(private dialog: MatDialog) {}
+
+  open() {
+    this.dialog.open(this.template);
+  }
+}
